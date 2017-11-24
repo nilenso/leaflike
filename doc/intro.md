93af3b5ef90f884e1937ce6dfd47f5eebdf83adc
namrata <chdhry.namrata@gmail.com>
2017-11-24 13:26:16 +0530

    init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bookmarks-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/bookmarks_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns bookmarks-clj.core-test
+  (:require [clojure.test :refer :all]
+            [bookmarks-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
