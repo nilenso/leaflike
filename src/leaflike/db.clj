06125754c5d028874a3367597a6da275e87fab69
namrata <chdhry.namrata@gmail.com>
2017-11-28 14:26:45 +0530

    remove create bookmark code

--- src/leaflike/db.clj
+++ src/leaflike/db.clj
@@ -2,7 +2,3 @@
   (:require [clojure.java.jdbc :as jdbc]))
 
 (defn db-spec [] {:connection-uri "jdbc:postgresql://localhost:5432/leaflike"})
-
-(defn create-bookmark
-  [data-map]
-  (jdbc/insert! (db-spec) :bookmarks data-map))
--- src/leaflike/routes.clj
+++ src/leaflike/routes.clj
@@ -1,22 +1,16 @@
 (ns leaflike.routes
   (:require [bidi.ring :as bidi]
-            [ring.util.response :as res]
-            [leaflike.db :as db]))
+            [ring.util.response :as res]))
 
 (defn yay [request]
   (res/response "Yay!"))
 
-(defn welcome [req]
+(defn welcome [request]
   (res/response "Welcome to leaflike"))
-
-(defn create-bookmark [req]
-  (let [body (:body req)]
-    (res/response (db/create-bookmark body))))
 
 (def urls
   {{:get "" welcome
-         "yay" yay}
-   {:post "create_bookmark" create-bookmark}})
+         "yay" yay}})
 
 (def handler
   (bidi/make-handler ["/" urls]))
