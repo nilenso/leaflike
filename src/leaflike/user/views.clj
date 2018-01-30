e46fb5a63f96bda4bebb2b07aaae153b5f8c699d
namrata <chdhry.namrata@gmail.com>
2018-01-30 18:10:48 +0530

    reduce width

--- src/leaflike/user/views.clj
+++ src/leaflike/user/views.clj
@@ -4,33 +4,35 @@
 
 (defn login-form
   []
-  [:div {:class "well"}
+  [:div {:id "content"}
    [:h3 {:class "text-success"} "Login"]
-   (f/form-to {:role "form" :novalidate ""}
-              [:post "/login"]
-              [:div {:class "form-group"}
-               (f/label {:class "control-label"} "username" "Username")
-               (f/text-field {:class "form-control" :placeholder "Username"} "username")]
-              [:div {:class "form-group"}
-               (f/label {:class "control-label"} "password" "Password")
-               (f/password-field {:class "form-control" :placeholder "Password"} "password")]
-              [:div {:class "form-group"}
-               (f/submit-button {:class "btn btn-primary"} "Login")])])
+   [:div {:class "well well-width"}
+    (f/form-to {:role "form" :novalidate ""}
+               [:post "/login"]
+               [:div {:class "form-group"}
+                (f/label {:class "control-label"} "username" "Username")
+                (f/text-field {:class "form-control" :placeholder "Username"} "username")]
+               [:div {:class "form-group"}
+                (f/label {:class "control-label"} "password" "Password")
+                (f/password-field {:class "form-control" :placeholder "Password"} "password")]
+               [:div {:class "form-group"}
+                (f/submit-button {:class "btn btn-primary"} "Login")])]])
 
 (defn signup-form
   []
-  [:div {:class "well"}
+  [:div {:id "content"}
    [:h3 {:class "text-success"} "Signup"]
-   (f/form-to {:role "form" :novalidate ""}
-              [:post "/signup"]
-              [:div {:class "form-group"}
-               (f/label {:class "control-label"} "email" "Email")
-               (f/email-field {:class "form-control" :placeholder "Email"} "email")]
-              [:div {:class "form-group"}
-               (f/label {:class "control-label"} "username" "Username")
-               (f/text-field {:class "form-control" :placeholder "Username"} "username")]
-              [:div {:class "form-group"}
-               (f/label {:class "control-label"} "password" "Password")
-               (f/password-field {:class "form-control" :placeholder "Password"} "password")]
-              [:div {:class "form-group"}
-               (f/submit-button {:class "btn btn-primary"} "Signup")])])
+   [:div {:class "well well-width"}
+    (f/form-to {:role "form" :novalidate ""}
+               [:post "/signup"]
+               [:div {:class "form-group"}
+                (f/label {:class "control-label"} "email" "Email")
+                (f/email-field {:class "form-control" :placeholder "Email"} "email")]
+               [:div {:class "form-group"}
+                (f/label {:class "control-label"} "username" "Username")
+                (f/text-field {:class "form-control" :placeholder "Username"} "username")]
+               [:div {:class "form-group"}
+                (f/label {:class "control-label"} "password" "Password")
+                (f/password-field {:class "form-control" :placeholder "Password"} "password")]
+               [:div {:class "form-group"}
+                (f/submit-button {:class "btn btn-primary"} "Signup")])]])
