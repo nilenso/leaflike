c47aac7df474178c541e010c77efa64f79651b47
Harsh Gupta <harsh@nilenso.com>
2018-03-16 16:42:38 +0530

    Make the extension chrome compatible

--- leaflike-ext/popup/add_bookmark.js
+++ leaflike-ext/popup/add_bookmark.js
@@ -2,20 +2,22 @@ console.log("Running the script");
 dev="http://localhost:8000";
 prod="https://leaflike.nilenso.com";
 leaflike_url = prod;
+
 function redirect(tabs) {
     console.log(tabs[0].url);
     console.log(tabs[0].title);
-    window.location = leaflike_url + "/bookmarks/add?url=" +
-	tabs[0].url + "&title=" + tabs[0].title +
+    redirect_url = leaflike_url + "/bookmarks/add?url=" +
+        tabs[0].url + "&title=" + tabs[0].title +
 	"&next=/status";
+
+    frame=document.getElementById('frame');
+    frame.setAttribute('src', redirect_url);
 }
 
 function reportError(error) {
     console.error(`Could not get the active tab: ${error}`);
 }
 
-browser.tabs.query({active: true, currentWindow: true})
-    .then(redirect)
-    .catch(reportError);
+chrome.tabs.query({active: true, currentWindow: true}, redirect);
 
 console.log("Finished with script");
